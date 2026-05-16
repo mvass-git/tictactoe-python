810f578436cdea53e586e50fc2f82a1119a696cb
mvass-git <158811490+mvass-git@users.noreply.github.com>
2026-05-16 14:37:29 +0300

    client base

---
+++ client.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.label import Label
+
+import json
+import threading
+import socket
